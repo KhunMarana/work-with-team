bdd51230277fa427489d4332d967ce46aedcdad0
Khun Aung Paing <54024040+KhunMarana@users.noreply.github.com>
2023-09-26 23:58:18 +0630

    first commit

---
+++ src/main/java/com/example/workwithteam/WorkWithTeamApplication.java
@@ -0,0 +1,13 @@
+package com.example.workwithteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkWithTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkWithTeamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workwithteam/WorkWithTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workwithteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkWithTeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
